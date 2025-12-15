c2591bf59a6fce51085d7031a6a68a06288e8ffe
chandup018 <chandupujari858@gmail.com>
2025-12-15 17:53:59 +0530

    Add files via upload
    added

---
+++ vot.js
@@ -0,0 +1,13 @@
+function vot()
+{
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var par=document.getElementById("par");
+
+    if(age>18){
+        alert("Elegible to Vote");
+    }else{
+        alert("you are mino");
+    }
+ 
+}
